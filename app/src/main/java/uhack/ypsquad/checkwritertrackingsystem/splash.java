67f58a621d9a6b3854a0571862f26268e7a132bc
JM Crisostomo <crisjohnmatthew13@gmail.com>
2017-11-30 06:32:54 +0800

    Version 0.1.2

--- app/src/main/java/uhack/ypsquad/checkwritertrackingsystem/splash.java
+++ app/src/main/java/uhack/ypsquad/checkwritertrackingsystem/splash.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.style.FoldingCube;
 
 public class splash extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        progressBar = (ProgressBar)findViewById(R.id.splash_progressBar);
+        FoldingCube foldingCube = new FoldingCube();
+        progressBar.setIndeterminateDrawable(foldingCube);
 
         int timerDelayed = 2;
         new Handler().postDelayed(new Runnable() {
@@ -18,7 +27,7 @@ public class splash extends AppCompatActivity {
                 startActivity(new Intent(splash.this, home.class));
                 finish();
             }
-        },timerDelayed * 1000);
+        },timerDelayed * 8000);
 //        Intent intent = new Intent(splash.this, home.class);
 //        startActivity(intent);
 
